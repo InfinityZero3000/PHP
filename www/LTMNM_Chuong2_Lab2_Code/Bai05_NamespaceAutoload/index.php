0ded55fb9541e6a2d1efd1ffa3838d0b9ac03adc
Nguyen Thang <nhthang312@gmail.com>
2026-07-08 16:18:22 +0700

    Remove all files from the Bai01_Car to Bai08_StudentManagement directories

--- www/LTMNM_Chuong2_Lab2_Code/Bai05_NamespaceAutoload/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/autoload.php';
-
-use App\Models\User;
-
-$user = new User();
-$user->sayHello();
--- www/LTMNM_Chuong2_Lab2_Code/Bai08_StudentManagement/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/autoload.php';
-
-use App\Students\Student;
-
-$student = new Student("Nguyen Huu Thang", 21, "2001234567");
-$student->displayInfo();
